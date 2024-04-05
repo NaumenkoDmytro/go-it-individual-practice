e7427c10c2b4aea8b9173441e9685847f5a340f7
NaumenkoDmytro <naymenko.dmitry5@gmail.com>
2024-04-05 15:20:10 +0300

    comments added

--- module1/Ex1/ListExercises.py
+++ module1/Ex1/ListExercises.py
@@ -33,11 +33,11 @@ print(largest_number_function([1,1111,2443,45,65,76,67]))
 #4. Write a Python program to get the smallest number from a list.
 
 def min_number_in_list(numbers: list):
-    for_smallest_number = numbers[0]
-    for num in numbers:
+    for_smallest_number = numbers[0] #Here we assign the integer type of data
+    for num in numbers: #Num here is also an integer, so can work with two integers
             if num < for_smallest_number: # if large number needed change the < sign to
-                for_smallest_number = num
-    return for_smallest_number
+                for_smallest_number = num #assign one integer to another
+    return for_smallest_number #and here we return integer
 
 print(min_number_in_list([1,1111,2443,45,65,76,67]))
 
