37b217aa0b00a1610342215cfa9abf6622f4741e
NaumenkoDmytro <naymenko.dmitry5@gmail.com>
2024-04-05 14:22:08 +0300

    Task 3 done

--- module1/Ex1/ListExercises.py
+++ module1/Ex1/ListExercises.py
@@ -22,5 +22,13 @@ print(multiplication_function([1,2,2,3]))
 #Done
 #3. Write a Python program to get the largest number from a list.
 
+def largest_number_function(numbers: list):
+    maximum_number = max(numbers)
+    return maximum_number
+
+print(largest_number_function([1,1111,2443,45,65,76,67]))
+
+#Done
+
 
 
--- module1/Ex1/exercises.py
+++ module1/Ex1/exercises.py
@@ -1,18 +1,10 @@
-def add_function(numbers_to_add : list):
-    total_add_sum = 0
-    for num in numbers_to_add:
-        total_add_sum +=num
+#3. Write a Python program to get the largest number from a list.
 
-    return total_add_sum
+def largest_number_function(numbers: list):
+    maximum_number = max(numbers)
+    return maximum_number
 
-def multiplication_function(numbers_to_multi : list):
-    total_multi_sum = 1 #always should be 1 becaouse it's multiplication
-    for num in numbers_to_multi:
-        total_multi_sum *= num
-    return total_multi_sum
-
-print(add_function([1,2,3]))
-print(multiplication_function([1,2,2,3]))
+print(largest_number_function([1,1111,2443,45,65,76,67]))
 
 
 
