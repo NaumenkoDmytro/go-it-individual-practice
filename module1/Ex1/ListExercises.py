74fd3382e3739620826f1c52d3ebc8a5379b76c9
NaumenkoDmytro <naymenko.dmitry5@gmail.com>
2024-04-08 10:10:53 +0300

    has-maps analyzed

--- module1/Ex1/ListExercises.py
+++ module1/Ex1/ListExercises.py
@@ -257,3 +257,22 @@ def remove_dublicates(elements:list) -> list:
 print(remove_dublicates([10, 20, 30, 20, 10, 50, 60, 40, 80, 50, 40]))
 
 #Done
+
+#Task: Given an array of string (strs), group the anagrams together. (hash-map)
+from collections import defaultdict #we innitializing this to get an empty dictionaries wil already assign values for "keys" so we don't need to create a key evry time to assign a values into the dictionary
+def groupAnagrams(strs: list[str]) -> list[str]:
+    anagram_map = defaultdict(list) # we created a dictionary by using the defauldict to add new keys with values without creating a key each time for new values and the type for value will be list (by default)
+    result = [] #an empty array that we will return as a result of function
+    
+    for s in strs: #here we are going trought each word in list
+        sorted_s = tuple(sorted(s)) #the main idea here is to sort all elements in word in alphabtic oreder and assign it's copy as a key into our dictionary(anagram_map), we convert it into the tuple because list(mutable) data type can't be the key (always have to be immutable) in dictionaries
+        anagram_map[sorted_s].append(s) #here we added they key to our (anagram map[sorted.s]), and add the word from the (strs) if it's match this key
+    
+    for values in anagram_map.values(): #will give us the list of the values
+        result.append(values) #and for each of this values we are going to append them into our values list and return it
+
+    return result
+
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+
+#analyzed, done with youtube.
--- module1/Ex1/exercises.py
+++ module1/Ex1/exercises.py
@@ -1,18 +1,15 @@
-#Task: Write the Python program to remove duplicates from a list to also preserve the original order of elements in the list.
-
-def remove_dublicates(elements:list) -> list:
-    dup_items = set() #here we create a list for duplicates
-    new_list = [] #here we create an empty list to store sorted data
+from collections import defaultdict #we innitializing this to get an empty dictionaries wil already assign values for "keys" so we don't need to create a key evry time to assign a values into the dictionary
+def groupAnagrams(strs: list[str]) -> list[str]:
+    anagram_map = defaultdict(list) # we created a dictionary by using the defauldict to add new keys with values without creating a key each time for new values and the type for value will be list (by default)
+    result = [] #an empty array that we will return as a result of function
     
+    for s in strs: #here we are going trought each word in list
+        sorted_s = tuple(sorted(s)) #the main idea here is to sort all elements in word in alphabtic oreder and assign it's copy as a key into our dictionary(anagram_map), we convert it into the tuple because list(mutable) data type can't be the key (always have to be immutable) in dictionaries
+        anagram_map[sorted_s].append(s) #here we added they key to our (anagram map[sorted.s]), and add the word from the (strs) if it's match this key
+    
+    for values in anagram_map.values(): #will give us the list of the values
+        result.append(values) #and for each of this values we are going to append them into our values list and return it
 
-    for elem in elements:
-        if elem not in dup_items:
-            new_list.append(elem)
-            dup_items.add(elem)
-        else:
-            new_list.append(" ")
-    return new_list
+    return result
 
-print(remove_dublicates([10, 20, 30, 20, 10, 50, 60, 40, 80, 50, 40]))
-
-
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
