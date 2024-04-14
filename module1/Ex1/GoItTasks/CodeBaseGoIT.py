b5702e3f94578a17d1ca343e3a4fe2939f1b80f9
NaumenkoDmytro <naymenko.dmitry5@gmail.com>
2024-04-14 11:37:47 +0300

    mentor task update

--- module1/Ex1/GoItTasks/CodeBaseGoIT.py
+++ module1/Ex1/GoItTasks/CodeBaseGoIT.py
@@ -490,3 +490,25 @@ print(string_to_date('2020.01.01'))
 
 #Done
 
+# В рядку написан текст. 
+# Для кожного слова підрахуйте 
+# кілька разів воно зустрічалось в цьому тексті.
+
+# text = '''
+#         Little girl, little girl Why are you crying? Inside your restless soul your heart is dying Little one, little one Your soul is purging Of love and razor blades Your blood is surging Runaway From the river to the street And find yourself with your face in the gutter Your a stray for the salvation army There is no place like home When you got no place to go Little girl, little girl Your life is calling The charlatans and saints of your abandon Little one little one The sky is falling The lifeboat of deception is now sailing In the wake all the way No rhyme or reason Your bloodshot eyes Will show your heart of treason Little girl little girl You dirty liar You're just a junkie Preaching to the choir Runaway To your lost tranquility And find yourself with your face in the gutter Your a stray for the dregs of humanity There is no place like home When you got no place to go The traces of blood Always follow you home Like the Mascara tears From your getaway Your walking with blisters And running with shears So unholy. Sister of grace. Runaway From the river to the street And find yourself with your face in the gutter You're a stray for the salvation army There is no place like home When you got no place to go
+#         '''
+# text = text.replace(',',' ').replace('.', ' ').replace('!', ' ').replace('?', ' ').lower()
+# word_counter = {}
+# text_splt = text.split()
+# for word in text_splt:
+#     word_counter[word] = word_counter.get(word, 0) + 1
+# # Перегорнули список    
+# word_counter_reversed = {}
+# for key, value in word_counter.items():
+#     word_counter_reversed[value] = word_counter_reversed.get(value, []) + [key]
+# # Вивели відсортований словник
+# sorted_words = sorted(word_counter_reversed)
+# for word in sorted_words:
+#     print(word,word_counter_reversed[word])
+
+# #Done
